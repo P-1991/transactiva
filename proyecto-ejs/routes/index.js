3198d90612049430c4003e0878492abf976c588e
paymen-system-poo <santiagopaulina352@gmail.com>
2025-06-26 07:54:05 +0000

    Add my views and my route siste

--- proyecto-ejs/routes/index.js
+++ proyecto-ejs/routes/index.js
@@ -1,9 +1,18 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "express" });
+});
+/* GET my views page. */
+router.get("/my-page", function (req, res, next) {
+  const myData = {
+    name: "paulina",
+    title: " my personal siste",
+    say: "hello!!",
+  };
+  res.render("myviews", { ...myData });
 });
 
 module.exports = router;
